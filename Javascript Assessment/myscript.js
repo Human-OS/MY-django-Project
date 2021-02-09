a7c2c6597226fe2b9a1a7c706eb7f0c45fde135e
Human-OS <si87868@gmail.com>
2021-02-10 01:03:26 +0600

    js array push pop prompt alert slice etc

--- Javascript Assessment/myscript.js
+++ Javascript Assessment/myscript.js
@@ -1,3 +1,41 @@
+var roster = []
+
+function addNew()
+{
+  var name = prompt("What name would you want to add")
+  roster.push(name)
+}
+function remove(){
+  var game = prompt("What name would you like to remove")
+  var index = roster.indexOf(game);
+  roster.splice(index,1)
+}
+function display()
+{
+  console.log(roster)
+}
+var start = prompt("Would You like to start? yes or no")
+var request ="empty";
+
+if (start === 'yes'){
+  while (request !== "quit") {
+    request = prompt("Please select an action: add, remove, display or quit")
+    if (request ==='add') {
+      addNew();
+    }else if (request === 'display') {
+      display();
+    }else if (request === 'remove') {
+      remove();
+    }else {
+      alert("Not recognised, will quit")
+    }
+  }
+}else {
+  console.log("thanks");
+}
+alert("thanks for co-operation")
+
+
 function sleepIn(weekday, vacation){
 return (!weekday || vacation)
 }
